2e1431c48f569296ffad7e81d122392cac1a0f55
Rohan Menezes <rohanmenezes@hnt-3-051-wlan.wireless-pennnet.upenn.edu>
2017-04-01 14:53:53 -0400

    fixed laundry

--- index.js
+++ index.js
@@ -75,17 +75,14 @@ function receivedMessage(event) {
     switch (messageText) {
     case 'laundry':
       axios('https://api.pennlabs.org/laundry/halls')
-      .then(({ response }) => {
+      .then(({ data }) => {
+        const response = data;
         const hallsArray = response['halls'];
         for (let i = 0; i < hallsArray.length; i++) {
           const dryers_available = hallsArray[i]['dryers_available'];
           const name = hallsArray[i]['name'];
-          const washers_available = hallsArray['washers_available'];
-
-          const ret =`
-          ${name} + : There are + ${dryers_available} + available dryers and  + ${washers_available}
-                + washers available!`;
-
+          const washers_available = hallsArray[i]['washers_available'];
+          const ret =`${name}: There are ${dryers_available} available dryers and ${washers_available} washers available!`;
           sendTextMessage(senderID, ret);
         }
       }).catch(err => {
